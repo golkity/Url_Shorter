135b29082c8e5d32df81891b2c097c98800959ec
Finnik <73883383+golkity@users.noreply.github.com>
2025-08-20 14:46:16 +0300

    fear(test & strcut): add any test for with function and fix bugs with provide and inin short url

--- src/app/command/creat_short_url.rs
+++ src/app/command/creat_short_url.rs
@@ -1,11 +1,44 @@
-pub struct CreateShortUrl;
+use crate::id_provider::IDProvider;
 
-impl CreateShortUrl{
+pub struct CreateShortUrl<I> where I: IDProvider{
+    id_provider: I
+}
+
+impl <I> CreateShortUrl <I> where I:IDProvider {
+    
+    pub fn new(id_provider: I) -> Self {
+        Self { id_provider }
+    }
     pub async fn execute(&self, full_url: String) -> Result<String, String> {
-       Ok("".to_owned())
+        let id = self.id_provider.provide();
+       Ok(id)
     }
 }
 
 
 #[cfg(test)]
-mod tests {}
+mod tests {
+   use super::*;
+
+   #[tokio::test]
+   async fn get_short_url() {
+       let id_provider = crate::id_provider::FakeIDProvider::new("123".to_owned());
+        let command = CreateShortUrl::new(id_provider);
+        
+        let result = command.execute("https://www.google.com".to_owned()).await;
+        
+        assert_ne!(result, Ok("".to_owned()));
+    }
+
+    #[tokio::test]
+    async fn get_rwo_different_short_url() {
+        let id_provider = crate::id_provider::FakeIDProvider::new("123".to_owned());
+        let command = CreateShortUrl::new(id_provider);
+
+        let result = command.execute("https://www.google.com".to_owned()).await;
+        let result2 =  command.execute("https://www.google.com".to_owned()).await;
+
+        assert_ne!(result, result2);
+    }
+
+}
