312bca9a5101382350a9643d32ca99a34b20272b
Finnik <73883383+golkity@users.noreply.github.com>
2025-08-20 17:17:50 +0300

    feat(command): fix busg +impl fun + linked dashmap + fix with generated nanoid + fix test

--- src/app/command/creat_short_url.rs
+++ src/app/command/creat_short_url.rs
@@ -1,16 +1,33 @@
 use crate::id_provider::IDProvider;
 
-pub struct CreateShortUrl<I> where I: IDProvider{
-    id_provider: I
+
+pub trait CreateShortUrlRepository {
+    fn save(&self, full_url: String, id: String) -> Result<(),String>;
 }
 
-impl <I> CreateShortUrl <I> where I:IDProvider {
-    
-    pub fn new(id_provider: I) -> Self {
-        Self { id_provider }
+pub struct CreateShortUrl <I, R>
+where
+    I: IDProvider,
+    R: CreateShortUrlRepository
+{
+    id_provider: I,
+    repo: R
+}
+
+impl <I,R> CreateShortUrl <I, R>
+where
+    I:IDProvider,
+    R:CreateShortUrlRepository
+{
+
+    pub fn new(id_provider: I, repo: R) -> Self {
+        Self { id_provider, repo }
     }
     pub async fn execute(&self, full_url: String) -> Result<String, String> {
         let id = self.id_provider.provide();
+
+        self.repo.save(full_url, id.clone())?;
+
        Ok(id)
     }
 }
@@ -18,12 +35,17 @@ impl <I> CreateShortUrl <I> where I:IDProvider {
 
 #[cfg(test)]
 mod tests {
-   use super::*;
+    use std::sync::Arc;
+    use dashmap::DashMap;
+    use crate::app::adapters::inmemory::InMemoryRepository;
+    use super::*;
 
    #[tokio::test]
    async fn get_short_url() {
        let id_provider = crate::id_provider::FakeIDProvider::new("123".to_owned());
-        let command = CreateShortUrl::new(id_provider);
+       let store = Arc::new(DashMap::new());
+       let repo = InMemoryRepository::new(store);
+        let command = CreateShortUrl::new(id_provider,repo);
         
         let result = command.execute("https://www.google.com".to_owned()).await;
         
@@ -32,10 +54,13 @@ mod tests {
 
     #[tokio::test]
     async fn get_rwo_different_short_url() {
-        let id_provider = crate::id_provider::FakeIDProvider::new("123".to_owned());
-        let command = CreateShortUrl::new(id_provider);
+        let idp = crate::id_provider::NanoIDProvider;
+        let store = Arc::new(DashMap::new());
+        let repo = InMemoryRepository::new(store);
+        let command = CreateShortUrl::new(idp, repo);
 
         let result = command.execute("https://www.google.com".to_owned()).await;
+
         let result2 =  command.execute("https://www.google.com".to_owned()).await;
 
         assert_ne!(result, result2);
